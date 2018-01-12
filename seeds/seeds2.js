fae9930170b87267329c5e3a16eb5db02f4794f8
Tiago Rodrigues <tmcrs123@gmail.com>
2018-01-12 23:11:22 +0000

    added api methods and customer list table

---
+++ seeds/seeds2.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const keys = require("../config/keys.js");
+const faker = require("faker");
+
+mongoose.Promise = global.Promise;
+
+if (process.env.NODE_ENV != "test") {
+  mongoose.connect(keys.mongoURI);
+}
+
+require("../models/Customer.js");
+
+const Customer = mongoose.model("customers");
+
+let customersSeed = [];
+
+for (i = 0; i < 50; i++) {
+  customersSeed.push({
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    phone: faker.phone.phoneNumber(),
+    blacklisted: faker.random.arrayElement([true, false]),
+    notes: faker.random.words(20)
+  });
+}
+
+Customer.insertMany(customersSeed).then(res => console.log(res));

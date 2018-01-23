e3c9b841f77765af26853071facb071a5dcb52ce
Tiago <tmcrs123@gmail.com>
2018-01-23 23:22:40 +0000

    deploy test

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,8 @@
 module.exports = {
-  mongoURI: process.env.MONGOURI
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookAppID: process.env.FACEBOOK_APP_ID,
+  facebookAppSecret: process.env.FACEBOOK_APP_SECRET
 };

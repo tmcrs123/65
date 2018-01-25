8279ab19d1e530f4e28efbde5d76aa61afad73e3
Tiago Miguel Costa Rodrigues <tmcrs123@gmail.com>
2018-01-25 11:06:33 +0000

    Delete test.js

--- config/test.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb://admin:admin@ds121534.mlab.com:21534/65react_test",
-  cookieKey: "1vsrkvmsrv95eaVSr124q32tDSRVSR@###!rcsrc#$$69",
-  googleClientID:
-    "239350865248-h0u3raq960amgc98b1bgnpqif8jp316h.apps.googleusercontent.com",
-  googleClientSecret: "76SAEcUuYKBivijIeOAkN2PU",
-  facebookAppID: "874620079373099",
-  facebookAppSecret: "f6014bd337d22ad66b03473bdb2ed00e"
-};
